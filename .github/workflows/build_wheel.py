67c46ba5d1731596778920378192f85bb317ef46
Ankur_Anand <Ankur.Anand@dell.com>
2023-10-30 18:56:02 +0530

    testing
    Signed-off-by: Ankur_Anand <Ankur.Anand@dell.com>

--- .github/workflows/build_wheel.py
+++ .github/workflows/build_wheel.py
@@ -22,7 +22,6 @@ command = [
     sys.executable,
 ]
 subprocess.run(command, check=True)
-os.chdir("..")
 wheels = [x for x in (ROOT / "target" / "wheels").iterdir()]
 if len(wheels) != 1:
     raise RuntimeError("expected one wheel, found " + repr(wheels))
