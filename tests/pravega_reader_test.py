9247ab8f6a56750e5327755da93e2c868daa3599
Ankur_Anand <Ankur.Anand@dell.com>
2024-01-31 16:47:51 +0530

    testing
    Signed-off-by: Ankur_Anand <Ankur.Anand@dell.com>

--- tests/pravega_reader_test.py
+++ tests/pravega_reader_test.py
@@ -85,8 +85,7 @@ class PravegaReaderTest(aiounittest.AsyncTestCase):
         w1.write_event("test event")
         w1.flush()
         # Create a reader Group Configuration to read from Tail of stream.
-        rg_config = pravega_client.StreamReaderGroupConfig(True, scope, stream)
-        reader_group=stream_manager.create_reader_group_with_config("rg" + suffix, scope, rg_config)
+        reader_group = stream_manager.create_reader_group("rg" + suffix, scope, stream, True)
         r1 = reader_group.create_reader("reader-1")
         print(repr(r1))
         w1.write_event("test event at Tail")
